0123a6228cf7f7a58219374c849533590b0e1fb0
pasadin-dot <68just68@gmail.com>
2025-12-08 14:23:56 +0800

    Added data directory exist calidation feature

--- fetch_fork.sh
+++ fetch_fork.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-BASE_DIR="/home/user/Coursework_1"
-FORK_DIR="/home/user/Coursework_1/fork_data"
-DATA_FILE="$BASE_DIR/try.txt"
+FORKDATA_DIR="./fork_data"
+DATA_FILE="./data.txt"
+
+if [ ! -d "$FORKDATA_DIR" ]; then
+	echo "'fork_data' directory does not exit. Creating it..."
+	mkdir -p "$FORKDATA_DIR"
+	echo "'fork_data' directory created."
+	echo ""
+fi
 
 while IFS= read -r REPO_NAME; do
 	REPO_NAME="$(echo "$REPO_NAME" | tr -d '\r\n' | xargs)"
@@ -10,6 +16,6 @@ while IFS= read -r REPO_NAME; do
 	REPO="${REPO_NAME##*/}"
 
 	echo "[+] Fetching forks from $REPO_NAME..."
-	grep "$OWNER" "$DATA_FILE" | awk '{print $4" "$5" "$6}' > "$FORK_DIR/$OWNER_$REPO_fork.txt"
+	grep "$OWNER" "$DATA_FILE" | awk '{print $4" "$5" "$6}' > "$FORKDATA_DIR/${OWNER}_${REPO}_fork.txt"
 
-done < "$BASE_DIR/repo_list.txt
+done < "./repo_list.txt"
--- fetch_star.sh
+++ fetch_star.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-BASE_DIR="/home/user/Coursework_1"
-STAR_DIR="/home/user/Coursework_1/star_data"
-DATA_FILE="$BASE_DIR/try.txt"
+STARDATA_DIR="./star_data"
+DATA_FILE="./data.txt"
+
+if [ ! -d "$STARDATA_DIR" ]; then
+	echo "'star_data' directory does not exist. Creating it..."
+	mkdir -p "$STARDATA_DIR"
+	echo "'star_data' directory created."
+	echo ""
+fi
 
 while IFS= read -r REPO_NAME; do
 	REPO_NAME="$(echo "$REPO_NAME" | tr -d '\r\n' | xargs)"
@@ -10,6 +16,6 @@ while IFS= read -r REPO_NAME; do
 	REPO="${REPO_NAME##*/}"
 
 	echo "[+] Fetching stars from $REPO_NAME..."
-	grep "$OWNER" "$DATA_FILE" | awk '{print $3" "$5" "$6}' > "$STAR_DIR/$OWNER_$REPO_star.txt"
+	grep "$OWNER" "$DATA_FILE" | awk '{print $3" "$5" "$6}' > "$STARDATA_DIR/${OWNER}_${REPO}_star.txt"
 
-done < "$BASE_DIR/repo_list.txt"
+done < "./repo_list.txt"
